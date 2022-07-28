f19e7b4114b6840d426f68c2278db06da79cb3aa
30isnottoolate <akos.varga.92@gmail.com>
2022-07-28 18:02:06 +0200

    Change json file address, read from file

--- src/script.js
+++ src/script.js
@@ -13,7 +13,10 @@ class Teleprompter extends React.Component {
       position: START_POS,
       direction: 0,
       speed: 1,
-      vault: "1234"
+      mode: "read", //read, select, set
+      vault: "1234",
+      numberOfTexts: 0,
+      currentText: "Loading..."
     };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -27,12 +30,12 @@ class Teleprompter extends React.Component {
     this.changeableInterval();
     document.addEventListener("keydown", this.handleKeyPress);
     
-    fetch("https://raw.githubusercontent.com/30isnottoolate/stage-teleprompter/main/liber.json")
+    fetch("https://raw.githubusercontent.com/30isnottoolate/misc/main/liber.json")
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       this.setState({
-        vault: data
+        vault: data,
+        currentText: data.texts.text_001.text
       })
     })
     .catch(() => console.log("Database missing."));
@@ -123,47 +126,7 @@ class Teleprompter extends React.Component {
         </div>
         <div id="slide" style={{top: this.state.position}} >
           <p id="text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. In 
-          iaculis nunc sed augue lacus viverra vitae. Facilisis mauris sit 
-          amet massa vitae tortor condimentum lacinia quis. Turpis egestas 
-          pretium aenean pharetra magna. Posuere morbi leo urna molestie at. 
-          Non curabitur gravida arcu ac tortor dignissim. Lectus urna duis 
-          convallis convallis tellus id interdum velit. Sagittis nisl rhoncus 
-          mattis rhoncus urna neque viverra. Odio facilisis mauris sit amet 
-          massa vitae. Enim ut sem viverra aliquet. Ipsum nunc aliquet bibendum
-          enim facilisis. Eu ultrices vitae auctor eu augue ut lectus arcu. Hac
-          habitasse platea dictumst quisque. Mus mauris vitae ultricies leo 
-          integer malesuada nunc vel. Eget nunc lobortis mattis aliquam 
-          faucibus purus in massa. Pellentesque nec nam aliquam sem et 
-          tortor consequat id.
-
-          Augue ut lectus arcu bibendum at varius vel. Et sollicitudin ac orci
-          phasellus egestas tellus rutrum tellus pellentesque. Sed pulvinar 
-          proin gravida hendrerit lectus a. Ultrices vitae auctor eu augue ut. 
-          Sed libero enim sed faucibus turpis. Convallis tellus id interdum 
-          velit laoreet. Facilisis magna etiam tempor orci eu lobortis 
-          elementum nibh tellus. Fusce id velit ut tortor pretium viverra. 
-          Convallis tellus id interdum velit laoreet. Justo donec enim diam 
-          vulputate ut pharetra sit. Commodo elit at imperdiet dui. Elit ut 
-          aliquam purus sit amet luctus venenatis lectus.
-
-          At tellus at urna condimentum mattis pellentesque id nibh. Pharetra 
-          diam sit amet nisl suscipit adipiscing. Sapien nec sagittis aliquam 
-          malesuada bibendum arcu. A pellentesque sit amet porttitor eget 
-          dolor morbi. Risus commodo viverra maecenas accumsan lacus. Nulla 
-          facilisi morbi tempus iaculis urna id volutpat lacus. Auctor neque 
-          vitae tempus quam pellentesque nec nam aliquam. Est sit amet 
-          facilisis magna etiam. Habitasse platea dictumst quisque sagittis 
-          purus sit. Adipiscing bibendum est ultricies integer quis auctor 
-          elit sed vulputate. Feugiat in ante metus dictum at tempor. Aliquam 
-          ut porttitor leo a diam sollicitudin tempor id. Nulla facilisi cras 
-          fermentum odio eu. Sit amet consectetur adipiscing elit pellentesque 
-          habitant. Semper risus in hendrerit gravida rutrum quisque non 
-          tellus. Porttitor lacus luctus accumsan tortor posuere ac ut 
-          consequat semper. Porttitor eget dolor morbi non arcu. Eros in 
-          cursus turpis massa tincidunt dui ut. Imperdiet proin fermentum leo 
-          vel. Sollicitudin nibh sit amet commodo nulla.
+          {this.state.currentText}
           </p>
         </div>
       </div>
