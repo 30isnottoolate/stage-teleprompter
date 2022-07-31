c4a7e540636dc20917df5b4f5cf944c604ccce33
30isnottoolate <akos.varga.92@gmail.com>
2022-07-31 19:44:53 +0200

    Add selection function
        Implemented text selection in TextList. A marker element moves to
        the position of the corresponding <li/>, based on currentIndex.

--- src/TextList.js
+++ src/TextList.js
@@ -5,6 +5,11 @@ class TextList extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+        currentIndex: 1,
+        markerPos: {
+          position: "absolute",
+          top: 38
+        }
       };
   
       this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -20,11 +25,45 @@ class TextList extends React.Component {
     
     handleKeyPress(event) {
       if (event.key === "c") {
-
+        if (this.state.currentIndex < this.props.state.textCount) {
+          this.setState((prevState) => ({
+            currentIndex: (prevState.currentIndex + 1),
+            markerPos: {
+              position: "absolute",
+              top: document.getElementById("li-" + (prevState.currentIndex + 1)).offsetTop
+            }
+          }));
+        } else {
+          this.setState(({
+            currentIndex: 1,
+            markerPos: {
+              position: "absolute",
+              top: document.getElementById("li-1").offsetTop
+            }
+          }));
+        }
       } else if (event.key === "b") {
+        if (this.state.currentIndex > 1) {
+          this.setState((prevState) => ({
+            currentIndex: (prevState.currentIndex - 1),
+            markerPos: {
+              position: "absolute",
+              top: document.getElementById("li-" + (prevState.currentIndex - 1)).offsetTop
+            }
+          }));
+        } else {
+          this.setState({
+            currentIndex: this.props.state.textCount,
+            markerPos: {
+              position: "absolute",
+              top: document.getElementById("li-" + this.props.state.textCount).offsetTop
+            }
+          });
+        }
+        
 
       } else if (event.key === "a") {
-
+        this.props.action(this.state.currentIndex, "read");
       }
     }
   
@@ -43,7 +82,7 @@ class TextList extends React.Component {
 
           for (const item in this.props.state.vault.texts) {
             index++;
-            list = list + "<li id=li-" + index + " >" + this.props.state.vault.texts[item].title + "</li>"
+            list = list + "<li id=li-" + index + " >" + this.props.state.vault.texts[item].title + "</li>";
           }
 
           return (
@@ -51,6 +90,7 @@ class TextList extends React.Component {
               <p>Welcome!</p>
               <p>Select a text.</p>
               <ul dangerouslySetInnerHTML={{__html: list}} />
+              <p style={this.state.markerPos}>&gt;&gt;&gt;</p>
               <div id="control">
                 <button id="button-a" >SELECT (A)</button>
                 <button id="button-b" >UP (B)</button>
@@ -72,6 +112,12 @@ class TextList extends React.Component {
           <div id="text-list">
             <p>Select a text.</p>
             <ul dangerouslySetInnerHTML={{__html: list}} />
+            <p style={this.state.markerPos}>&gt;&gt;&gt;</p>
+            <div id="control">
+              <button id="button-a" >SELECT (A)</button>
+              <button id="button-b" >UP (B)</button>
+              <button id="button-c" >DOWN (C)</button>
+            </div>
           </div>
         )
       }
