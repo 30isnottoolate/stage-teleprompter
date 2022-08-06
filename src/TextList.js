27c7ff69ab78f90d7a0cf3bb4a7588bdc95d6566
30isnottoolate <akos.varga.92@gmail.com>
2022-08-06 13:01:07 +0200

    Implement simplified changeIndex
        List position (listPos) is now calculated before every render.
    
        Implemented simplified changeIndex.

--- src/TextList.js
+++ src/TextList.js
@@ -4,9 +4,6 @@ import './Teleprompter.css';
 class TextList extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        
-      };
   
       this.handleKeyPress = this.handleKeyPress.bind(this);
     }
@@ -22,23 +19,25 @@ class TextList extends React.Component {
     handleKeyPress(event) {
       if (event.key === "c") {
         if (this.props.state.currentIndex < this.props.state.textCount) {
-          this.props.index(1);
+          this.props.index(this.props.state.currentIndex + 1);
         } else {
-          this.props.index(1 - this.props.state.textCount);
+          this.props.index(1);
         }
       } else if (event.key === "b") {
         if (this.props.state.currentIndex > 1) {
-          this.props.index(-1);
+          this.props.index(this.props.state.currentIndex - 1);
         } else {
-          this.props.index(this.props.state.textCount - 1);
+          this.props.index(this.props.state.textCount);
         }
         
       } else if (event.key === "a") {
-        this.props.action(this.props.state.currentIndex, "read", this.state.markerPos);
+        this.props.mode("read");
       }
     }
   
     render() {
+      let listPos = (2 - this.props.state.currentIndex) * this.props.state.fontSize * this.props.state.lineHeight;
+
       if (this.props.state.mode === "start" || this.props.state.mode === "select") {
         if (this.props.state.textCount === 0) {
           return (
@@ -51,16 +50,16 @@ class TextList extends React.Component {
           let list = "";
           let index = 0;
 
-          for (const item in this.props.state.vault.texts) {
+          for (const item in this.props.state.data.texts) {
             index++;
-            list = list + `<li id="li-${index}" style="padding-left:${this.props.state.fontSize*0.69}px">${this.props.state.vault.texts[item].title}</li>`;
+            list = list + `<li id="li-${index}" style="padding-left:${this.props.state.fontSize * 0.69}px">${this.props.state.data.texts[item].title}</li>`;
           }
 
           return (
             <div id="text-list" style={{fontSize: this.props.state.fontSize, color: this.props.state.uiColor}}>
               <p id="head-line" className={this.props.state.currentIndex === 1 ? "visible" : "hidden"}>SELECT:</p>
-              <ul dangerouslySetInnerHTML={{__html: list}} style={{position: "absolute", top: (2 - this.props.state.currentIndex) * this.props.state.fontSize * this.props.state.lineHeight}}/>
-              <p id="text-marker" style={{paddingLeft: (this.props.state.fontSize*0.19)+"px"}}>&#129170;</p>
+              <ul dangerouslySetInnerHTML={{__html: list}} style={{position: "absolute", top: listPos}}/>
+              <p id="text-marker" style={{paddingLeft: (this.props.state.fontSize * 0.19) + "px"}}>&#129170;</p>
               <div id="control">
                 <button id="button-a" style={{color: this.props.state.uiColor, borderColor: this.props.state.uiColor}}>SELECT (A)</button>
                 <button id="button-b" style={{color: this.props.state.uiColor, borderColor: this.props.state.uiColor}}>UP (B)</button>
