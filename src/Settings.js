6fdd4f31fd6564f97cb9ba0038c2e69e08e3389c
30isnottoolate <akos.varga.92@gmail.com>
2022-08-09 19:07:37 +0200

    Initialize file

---
+++ src/Settings.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Teleprompter.css';
+
+class Settings extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  
+    }
+  
+    render() {
+      return (
+        <div id="settings">
+            <p>SETTINGS:</p>
+        </div>
+      )
+    }
+  }
+
+export default Settings;
