51264d917ee751c31637d6e7ea5711ca61bd4c40
30isnottoolate <akos.varga.92@gmail.com>
2022-09-12 18:25:46 +0200

    Fill in handleKeyPress, handleKeyHold for A,B,C

--- src/StartHelp.js
+++ src/StartHelp.js
@@ -5,7 +5,9 @@ class StartHelp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      helpIndex: 1,
+      keyHold: false,
+      keyDownTime: ""
     };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -25,12 +27,66 @@ class StartHelp extends React.Component {
     document.removeEventListener("keyup", this.handleKeyHold);
   }
 
-  handleKeyPress() {
-
+  handleKeyPress(event) {
+    this.setState((prevState) => {
+      if (!prevState.keyHold) {
+        if (event.key === "a") {
+          return {
+            keyHold: true,
+            keyDownTime: (new Date()).getTime()
+          }
+        } else if (event.key === "b") {
+          return {
+            keyHold: true,
+            keyDownTime: (new Date()).getTime()
+          }
+        } else if (event.key === "c") {
+          if (this.state.helpIndex < 7) {
+            return {
+              helpIndex: prevState.helpIndex + 1
+            }
+          } else {
+            return {
+              helpIndex: 1
+            }
+          }
+        }
+      }
+    });
   }
 
-  handleKeyHold() {
-
+  handleKeyHold(event) {
+    this.setState((prevState) => {
+      if (prevState.keyHold) {
+        if (event.key === "a") {
+          if (((new Date()).getTime() - this.state.keyDownTime) > this.props.state.holdButtonTime) {
+            this.props.mode("set");
+            return {
+              keyHold: false,
+              keyDownTime: ""
+            }
+          } else {
+            return {
+              keyHold: false,
+              keyDownTime: ""
+            }
+          }
+        } else if (event.key === "b") {
+          if (((new Date()).getTime() - this.state.keyDownTime) > this.props.state.holdButtonTime) {
+            this.props.mode("select");
+            return {
+              keyHold: false,
+              keyDownTime: ""
+            }
+          } else {
+            return {
+              keyHold: false,
+              keyDownTime: ""
+            }
+          }
+        }
+      }
+    });
   }
 
   handleButtonA() {
@@ -48,7 +104,7 @@ class StartHelp extends React.Component {
   render() {
     return (
       <div id="text-list" className={this.props.state.orientation === "vertical" ? "rotate-cw" : ""} style={{ fontSize: this.props.state.fontSize, color: this.props.state.uIColor, lineHeight: this.props.state.lineHeight }}>
-        <p id="head-line">KV Teleprompter</p>
+        <p id="head-line" className={this.props.state.helpIndex === 1 ? "visible" : "hidden"}>KV Teleprompter</p>
         <div id="control">
           <button id="button-a" style={{ color: this.props.state.uIColor, borderColor: this.props.state.uIColor }} onClick={this.handleButtonA} >&#8984;</button>
           <button id="button-b" style={{ color: this.props.state.uIColor, borderColor: this.props.state.uIColor }} onClick={this.handleButtonB} >&#9636;</button>
