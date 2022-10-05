ec16309babdeb7874b2e18ca656ba7070d523756
30isnottoolate <akos.varga.92@gmail.com>
2022-10-05 19:37:32 +0200

    Delete colors from Settings state

--- src/Settings.js
+++ src/Settings.js
@@ -8,14 +8,7 @@ class Settings extends React.Component {
       settingsIndex: 1,
       inChangeMode: false,
       keyHold: false,
-      keyDownTime: "",
-      colors: {
-        1: {"name": "red", "code": "#ffd6d9"},
-        2: {"name": "green", "code": "#b4f8ff"},
-        3: {"name": "blue", "code": "#99d3ff"},
-        4: {"name": "white", "code": "#ffffff"},
-        5: {"name": "yellow", "code": "#fff4ad"},
-      }
+      keyDownTime: ""
     };
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
