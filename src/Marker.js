de03ded79f8fb71b6055d182fcb50347d31b6703
30isnottoolate <akos.varga.92@gmail.com>
2022-11-04 19:03:55 +0100

    Add new component

---
+++ src/Marker.js
@@ -0,0 +1,15 @@
+class Marker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <button>
+
+            </button>
+        );
+    }
+}
+
+export default Marker;
