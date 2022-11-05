6b6abc8918329321a8f8617ea5bf2786240687d2
30isnottoolate <akos.varga.92@gmail.com>
2022-11-05 16:24:42 +0100

    Create new component

---
+++ src/components/Marker.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Marker extends React.Component {
+    render() {
+        return (
+            <svg
+                id="text-marker"
+                height={this.props.fontSize}
+                fill={this.props.stateColor}
+                style={{boxSizing: "content-box", top: this.props.top, left: this.props.left, padding: `${this.props.fontSize * (this.props.lineHeight - 1) / 2} 0`}}
+                viewBox="0 0 57 150">
+                <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z"/>
+            </svg>
+        );
+    }
+}
+
+export default Marker;
