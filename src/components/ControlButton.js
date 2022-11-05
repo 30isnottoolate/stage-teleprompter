936bf7ac5ca8f6fd238e0b792e2ff9cdad008067
30isnottoolate <akos.varga.92@gmail.com>
2022-11-05 11:17:46 +0100

    Rename component

---
+++ src/components/ControlButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const icons = {
+    up: "",
+    down: "",
+    left: "",
+    right: "",
+    play: "",
+    pause: "",
+    next: "",
+    list: "",
+    settings: "",
+    selectSettings: "",
+    selectList: ""
+}
+
+class ControlButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <button>
+
+            </button>
+        );
+    }
+}
+
+export default ControlButton;
