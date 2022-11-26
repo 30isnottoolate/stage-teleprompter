01b6d292259fdb6405269be2d44313bc38d9b24e
30isnottoolate <akos.varga.92@gmail.com>
2022-11-26 17:11:28 +0100

    Create separate file for colors

---
+++ src/utilities/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+	1: { "name": "red", "code": "#ffd6d9" },
+	2: { "name": "green", "code": "#b4f8ff" },
+	3: { "name": "blue", "code": "#99d3ff" },
+	4: { "name": "white", "code": "#ffffff" },
+	5: { "name": "yellow", "code": "#fff4ad" },
+}
+
+export default colors;
