ac859e214c79f047e46e2447fe040c16e5bd8556
30isnottoolate <akos.varga.92@gmail.com>
2022-12-14 17:50:05 +0100

    Rework color profile

--- src/utilities/colors.js
+++ src/utilities/colors.js
@@ -1,9 +1,9 @@
 const colors = {
-	1: { "name": "red", "code": "#ffd6d9" },
-	2: { "name": "green", "code": "#b4f8ff" },
-	3: { "name": "blue", "code": "#99d3ff" },
-	4: { "name": "white", "code": "#ffffff" },
-	5: { "name": "yellow", "code": "#fff4ad" },
+    1: { "name": "red", "code": "#fecaca" },
+    2: { "name": "green", "code": "#bbf7d0" },
+    3: { "name": "blue", "code": "#7dd3fc" },
+    4: { "name": "white", "code": "#f5f5f5" },
+    5: { "name": "yellow", "code": "#fef9c3" },
 }
 
 export default colors;
