cf2169124de00607df94a65d62b5b90e353c3c72
30isnottoolate <akos.varga.92@gmail.com>
2022-12-15 16:35:10 +0100

    Create new component

---
+++ src/components/Image.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import icons from '../utilities/icons';
+
+class Image extends React.Component {
+    render() {
+        let padding = this.props.fontSize * (this.props.lineHeight - 1) / 2;
+
+        return (
+            <svg
+                className="status-image"
+                height={2 * this.props.fontSize}
+                fill={this.props.stateColor}
+                style={{
+                    padding: `${4 * padding}px 0`
+                }}
+                viewBox="0 0 16 16">
+                {icons[this.props.icon]}
+            </svg>
+        );
+    }
+}
+
+export default Image;
