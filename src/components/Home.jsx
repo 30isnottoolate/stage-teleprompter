e46df36b4df2a3e47b326561b1803fb7129d41bc
30isnottoolate <akos.varga.92@gmail.com>
2022-12-15 16:35:41 +0100

    Import and add Images, overhaul component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import colors from '../utilities/colors';
+import Image from './Image';
 import ControlButton from './ControlButton';
 
 class Home extends React.Component {
@@ -33,7 +34,7 @@ class Home extends React.Component {
 	handleButtonCInfo = () => this.props.changeMode("info");
 
 	render() {
-		const { settings } = this.props;
+		const { settings, libraryStatus } = this.props;
 
 		let stateColor = colors[settings.colorIndex].code;
 		let responsiveWidth = (settings.orientation === "vertical") ? "100vh" : "100vw";
@@ -48,9 +49,45 @@ class Home extends React.Component {
 					lineHeight: settings.lineHeight
 				}}>
 				<p
-					className="title">
+					className="title"
+					style={{ paddingTop: 0.25 * settings.fontSize * settings.lineHeight }} >
 					KV Stage Teleprompter
 				</p>
+				<div className="content">
+					{libraryStatus === "checking" &&
+						<Image
+							icon="checking"
+							fontSize={settings.fontSize}
+							lineHeight={settings.lineHeight}
+							stateColor={stateColor}
+						/>}
+					{libraryStatus === "valid" &&
+						<Image
+							icon="valid"
+							fontSize={settings.fontSize}
+							lineHeight={settings.lineHeight}
+							stateColor={stateColor}
+						/>}
+					{libraryStatus === "invalid" &&
+						<Image
+							icon="invalid"
+							fontSize={settings.fontSize}
+							lineHeight={settings.lineHeight}
+							stateColor={stateColor}
+						/>}
+					{libraryStatus === "missing" &&
+						<Image
+							icon="missing"
+							fontSize={settings.fontSize}
+							lineHeight={settings.lineHeight}
+							stateColor={stateColor}
+						/>}
+					<p style={{ fontSize: settings.fontSize * 0.75 }}>{libraryStatus === "checking" ? "CHECKING LIBRARY..." :
+						libraryStatus === "missing" ? "LIBRARY MISSING" :
+							libraryStatus === "invalid" ? "LIBRARY INVALID" :
+								"LIBRARY READY"
+					}</p>
+				</div>
 				<div id="control" style={{ width: responsiveWidth }}>
 					<ControlButton
 						fontSize={settings.fontSize}
